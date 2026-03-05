6548551b74e84acd67ed81a87bfd4faf0757b6a9
Jim495217 <jimmel.chambers.jr@uhsp.edu>
2026-03-05 12:23:44 -0600

    Fixed main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import NotFound from "./components/NotFound";
+
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import MoviePage from "./pages/MoviePage";
+import FavoritesPage from "./pages/FavoritesPage";
+
 function App() {
-  return <h1>Movie Reviewer Working</h1>;
+  return (
+    <div className="app">
+      <Navbar />
+
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/movie/:id" element={<MoviePage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
